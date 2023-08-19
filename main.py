6e9652f3ef0d2b7ab6c696730a74fe2f1b3a5909
Hana Ghanem <hana.ghanem@gmail.com>
2023-08-19 03:08:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#TODO Calculator FUNCTIONS
+
+
+
+
+
+
+def calculate(a, b, operation):
+        #TODO (CONTINUE ON THE SAME PATTERN
+
+
+
+
+num1 = #TODO INPUT FROM USER
+num2 = #TODO INPUT FROM USER
+op = #TODO INPUT FROM USER
+print(calculate(num1, num2, op))
