9fb7f68052ed2a7abf9ad08001668381cc01aed3
unknown <menaelaskary5@gmai.com>
2023-08-22 01:20:01 +0300

    calculator

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 num1 = int (input ("Enter the number:"))
 num2 = int(input("Enter the number:"))
 op = int(input("please enter a number endicating an  operation \n 1)addition \n 2)subtraction \n 3)multiplication \n 4) division"))
-def calculate(num1, num2, op):
+def calculate(num1, num2, op):   
         if op==2:
-          return num1-num2
-print(calculate(num1, num2, op))
+          return num1-num2  
+        elif op==1:
+          return num1+num2
+        elif op==3:
+          return num1*num2
+        elif op==4:
+          return num1/num2
+        print(calculate(num1, num2, op))
+        
