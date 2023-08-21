e277231e926c9024dbce9e22d5da7e497484cee5
unknown <menaelaskary5@gmai.com>
2023-08-21 22:55:38 +0300

    mennas part:-

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-#TODO Calculator FUNCTIONS
+num1 = int (input("Enter the number:"))
+num2 = int (input("Enter the number:"))
+op = int(input("please enter a number endicating an  operation \n 1)addition \n 2)subtraction \n 3)multiplication \n 4) division"))
 
-
-
-
-
-
-def calculate(a, b, operation):
-        #TODO (CONTINUE ON THE SAME PATTERN
-
-
-
-
-num1 = #TODO INPUT FROM USER
-num2 = #TODO INPUT FROM USER
-op = #TODO INPUT FROM USER
+def calculate(num1, num2, op):
+        if op==2:
+             return num1-num2
 print(calculate(num1, num2, op))
